d521b55a9e92d80cfedf7c6b3a1a2725c5a7719f
jack201 <zwy831@163.com>
2019-07-23 10:19:31 +0800

    新增dubbo test consumer单元测试示例

--- soul-test/soul-test-dubbo/soul-test-dubbo-consumer/src/test/java/org/dromara/soul/test/dubbo/consumer/service/SoulDubboSericeTest.java
+++ soul-test/soul-test-dubbo/soul-test-dubbo-consumer/src/test/java/org/dromara/soul/test/dubbo/consumer/service/SoulDubboSericeTest.java
@@ -3,6 +3,8 @@ package org.dromara.soul.test.dubbo.consumer.service;
 import static org.junit.Assert.*;
 
 import cn.hutool.core.util.RandomUtil;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.dromara.soul.test.dubbo.api.entity.DubboTest;
 import org.junit.Test;
@@ -29,6 +31,41 @@ public class SoulDubboSericeTest {
     dubboTest.setName(RandomUtil.randomString(10));
     soulDubboSerice.insert(dubboTest);
   }
+
+  @Test
+  public void insert3() {
+    DubboTest dubboTest = new DubboTest();
+    dubboTest.setId("1111111");
+    dubboTest.setName(RandomUtil.randomString(10));
+    String id = RandomUtil.randomNumbers(10);
+    String name = RandomUtil.randomString(10);
+    soulDubboSerice.insert3(dubboTest,id,name);
+  }
+
+  @Test
+  public void findByIdAndName() {
+    String id = RandomUtil.randomNumbers(10);
+    String name = RandomUtil.randomString(10);
+    soulDubboSerice.findByIdAndName(id,name);
+  }
+
+
+  @Test
+  public void testEntityStringListParam() {
+    DubboTest dubboTest = new DubboTest();
+    dubboTest.setId("1111111");
+    dubboTest.setName(RandomUtil.randomString(10));
+    String id = RandomUtil.randomNumbers(10);
+    List<String> listParams = new ArrayList<>();
+    listParams.add(RandomUtil.randomString(10));
+    listParams.add(RandomUtil.randomString(10));
+    soulDubboSerice.testEntityStringListParam(dubboTest,id,listParams);
+
+  }
+
+
+
+
 
 
 }
